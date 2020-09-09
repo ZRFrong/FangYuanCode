e258ec60570fb6f67b16543f1cce36da3af019f0
59zheng <903954913@qq.com>
2020-09-09 18:25:23 +0800

    tcp模块和api模块controller和domain包中swagger注解添加

--- ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/json/JSONUtils.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/json/JSONUtils.java
@@ -1,13 +1,14 @@
 package com.ruoyi.common.json;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Component
 @Slf4j
@@ -35,20 +36,17 @@ public class JSONUtils <T,V> {
         return s;
     }
 
+    /**
+     * list 转json数组
+     * @param data
+     * @return
+     */
+    public String listToJsonArray(List data){
+        JSONArray array = JSONArray.parseArray(JSON.toJSONString(data));
 
-
-
-    public static void main(String[] args) throws JsonProcessingException {
-        JSONUtils<String, String> jsonUtils = new JSONUtils<>();
-        HashMap<String, String> map = new HashMap<>();
-        map.put("code", "1234");
-        String s = obj.writeValueAsString(map);
-        System.out.println(s);
-        System.out.println(obj.writeValueAsString(map));
-        System.out.println(jsonUtils.mapToString(map));
-//        String s ="{\"Message\":\"OK\",\"RequestId\":\"7402B09A-F12E-4655-96DF-8F053F6D5CAD\",\"BizId\":\"975423199111996661^0\",\"Code\":\"OK\"}";
-//        JSONUtils<String, String> jsonUtils = new JSONUtils<>();
-//        Map<String, String> map = jsonUtils.stringToMap(s);
-//        System.out.println(map.get("Code"));
+        return array.toJSONString();
     }
+
+
+
 }
