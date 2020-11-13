4e02f734ebfadbd6061e6df6a5a1d638cfcbc686
ZHAO <769952286@qq.com>
2020-11-13 09:51:16 +0800

    提交跨域配置文件，

--- ruoyi-gateway/src/main/java/com/ruoyi/gateway/config/GlobalCorsConfig.java
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/config/GlobalCorsConfig.java
@@ -7,17 +7,19 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 
-//@Configuration
+@Configuration
 public class GlobalCorsConfig {
 
-  //  @Bean
+    @Bean
     public CorsFilter corsFilter() {
         //1.添加CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
         //1) 允许的域,不要写*，否则cookie就无法使用了
         //添加白名单
-
+        config.addAllowedMethod("*");
         config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        //config.addAllowedOrigin("*");
         //2) 是否发送Cookie信息
         config.setAllowCredentials(true);
         //3) 允许的请求方式
@@ -35,7 +37,6 @@ public class GlobalCorsConfig {
         //2.添加映射路径，我们拦截一切请求
         UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
         configSource.registerCorsConfiguration("/**", config);
-
         return new CorsFilter(configSource);
     }
 
