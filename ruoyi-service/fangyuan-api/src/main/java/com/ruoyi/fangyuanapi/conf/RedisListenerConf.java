986234a4de539e520bdf6944e2484fe3ae0b4475
zheng <903954913@qq.com>
2021-06-01 15:50:25 +0800

    redis监听配置提交

---
+++ ruoyi-service/fangyuan-api/src/main/java/com/ruoyi/fangyuanapi/conf/RedisListenerConf.java
@@ -0,0 +1,23 @@
+package com.ruoyi.fangyuanapi.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+/**
+ * @Description: redis监听配置类
+ * @Author zheng
+ * @Date 2021/5/31 11:29
+ * @Version 1.0
+ */
+@Configuration
+public class RedisListenerConf {
+
+    @Bean
+    public RedisMessageListenerContainer messageListener(RedisConnectionFactory connectionFactory){
+        RedisMessageListenerContainer redisMessageListenerContainer = new RedisMessageListenerContainer ();
+        redisMessageListenerContainer.setConnectionFactory(connectionFactory);
+        return redisMessageListenerContainer;
+    }
+}
