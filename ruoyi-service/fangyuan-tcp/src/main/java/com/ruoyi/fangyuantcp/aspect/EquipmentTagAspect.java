55e0c139d4435662a08932d82747943abf7c8074
zheng <903954913@qq.com>
2021-06-16 09:40:38 +0800

    生成传感器tag数据修改

--- ruoyi-service/fangyuan-tcp/src/main/java/com/ruoyi/fangyuantcp/aspect/EquipmentTagAspect.java
+++ ruoyi-service/fangyuan-tcp/src/main/java/com/ruoyi/fangyuantcp/aspect/EquipmentTagAspect.java
@@ -9,6 +9,7 @@ package com.ruoyi.fangyuantcp.aspect;
  **/
 
 import cn.hutool.core.collection.CollectionUtil;
+import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.fangyuantcp.mapper.DbTcpTypeMapper;
 import com.ruoyi.system.domain.DbTcpType;
 import lombok.extern.slf4j.Slf4j;
@@ -18,8 +19,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 @Aspect
 @Component
@@ -39,20 +42,74 @@ public class EquipmentTagAspect {
      */
     @Around("@annotation(equipmentTag)")
     public Object doProccess(ProceedingJoinPoint proceedingJoinPoint, EquipmentTag equipmentTag) throws Throwable {
-        final List<DbTcpType> dbTcpTypeList = (List<DbTcpType>)proceedingJoinPoint.proceed();
-        log.info("EquipmentTagAspect.doProccess dbTcpTypeList:{}",dbTcpTypeList);
-        if(DEFAULT_TYPE.equals(equipmentTag.type()) && CollectionUtil.isEmpty(dbTcpTypeList)){
+        List<DbTcpType> dbTcpTypeList = (List<DbTcpType>)proceedingJoinPoint.proceed();
+        log.info("EquipmentTagAspect.doProccess dbTcpTypeList:{}", dbTcpTypeList);
+        if ("tag".equals(equipmentTag.type()) && CollectionUtil.isEmpty(dbTcpTypeList)) {
             Map<String, Object> params = FeedbackInterceptAspect.getMap(proceedingJoinPoint);
-            DbTcpType dbTcpType = (DbTcpType) params.get("dbTcpType");
-            try{
-                List<DbTcpType> dbTcpTypeTagList = dbTcpTypeMapper.selectDbTcpTypeTagList(dbTcpType);
-                log.info("EquipmentTagAspect.doProccess dbTcpTypeTagList:{}",dbTcpTypeTagList);
-                if(CollectionUtil.isNotEmpty(dbTcpTypeTagList))
-                    return dbTcpTypeTagList;
-            }catch (Exception e){
-                log.error("EquipmentTagAspect.doProccess error:{}",e);
+            DbTcpType dbTcpType = (DbTcpType)params.get("dbTcpType");
+            try {
+                List<DbTcpType> dbTcpTypeTagList = this.dbTcpTypeMapper.selectDbTcpTypeTagList(dbTcpType);
+                log.info("EquipmentTagAspect.doProccess dbTcpTypeTagList:{}", dbTcpTypeTagList);
+                if (CollectionUtil.isNotEmpty(dbTcpTypeTagList))
+                    return assemblyData(dbTcpTypeTagList);
+            } catch (Exception e) {
+                log.error("EquipmentTagAspect.doProccess error:{}", e);
             }
         }
         return dbTcpTypeList;
+    }
+
+    private List<DbTcpType> assemblyData(List<DbTcpType> dbTcpTypeList) {
+        dbTcpTypeList.forEach(tcpType -> {
+            if (StringUtils.isNotEmpty(tcpType.getTemperatureSoil()))
+                tcpType.setTemperatureSoil(randomData(tcpType.getTemperatureSoil()));
+            if (StringUtils.isNotEmpty(tcpType.getHumiditySoil()))
+                tcpType.setHumiditySoil(randomData(tcpType.getHumiditySoil()));
+            if (StringUtils.isNotEmpty(tcpType.getLight()))
+                tcpType.setLight(randomData(tcpType.getLight()));
+            if (StringUtils.isNotEmpty(tcpType.getCo2()))
+                tcpType.setCo2(randomData(tcpType.getCo2()));
+            if (StringUtils.isNotEmpty(tcpType.getTemperatureAir()))
+                tcpType.setTemperatureAir(randomData(tcpType.getTemperatureAir()));
+            if (StringUtils.isNotEmpty(tcpType.getHumidityAir()))
+                tcpType.setHumidityAir(randomData(tcpType.getHumidityAir()));
+            if (StringUtils.isNotEmpty(tcpType.getConductivity()))
+                tcpType.setConductivity(randomData(tcpType.getConductivity()));
+            if (StringUtils.isNotEmpty(tcpType.getPh()))
+                tcpType.setPh(randomData(tcpType.getPh()));
+            if (StringUtils.isNotEmpty(tcpType.getNitrogen()))
+                tcpType.setNitrogen(randomData(tcpType.getNitrogen()));
+            if (StringUtils.isNotEmpty(tcpType.getPhosphorus()))
+                tcpType.setPhosphorus(randomData(tcpType.getPhosphorus()));
+            if (StringUtils.isNotEmpty(tcpType.getPotassium()))
+                tcpType.setPotassium(randomData(tcpType.getPotassium()));
+        });
+        return dbTcpTypeList;
+    }
+
+    private final DecimalFormat dataFormat = new DecimalFormat("#.0");
+
+    private static final float FLOAT_VOLATILITY = 0.5F;
+
+    private static final int INT_VOLATILITY = 3;
+
+    private String randomData(String dataVale) {
+        Random random = new Random();
+        if (dataVale.contains(".")) {
+            Float aFloat = Float.valueOf(dataVale);
+            float f1 = aFloat - FLOAT_VOLATILITY;
+            float f2 = aFloat + FLOAT_VOLATILITY;
+            if (f1 <= 0.0F)
+                f1 = aFloat;
+            float f3 = f1 + (f2 - f1) * (new Random()).nextFloat();
+            return (f3 < 1.0F) ? String.valueOf(f3).substring(0, 3) : String.valueOf(this.dataFormat.format(f3));
+        }
+        Integer dataInt = Integer.valueOf(dataVale);
+        int minValue = dataInt - INT_VOLATILITY;
+        int maxValue = dataInt + INT_VOLATILITY;
+        if (minValue <= 0)
+            minValue = dataInt;
+        int finalValue = random.nextInt(maxValue) % (maxValue - minValue + 1) + minValue;
+        return String.valueOf(finalValue);
     }
 }
