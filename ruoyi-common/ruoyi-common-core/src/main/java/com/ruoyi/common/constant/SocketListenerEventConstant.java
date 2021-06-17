5f2137fd2c40b33471ac67eb8177faf5edc95b1a
zheng <903954913@qq.com>
2021-06-17 16:09:03 +0800

    生成传感器tag数据修改

---
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/constant/SocketListenerEventConstant.java
@@ -0,0 +1,29 @@
+package com.ruoyi.common.constant;
+
+/**
+ * @Description: socket通道监听事件枚举
+ * @Author zheng
+ * @Date 2021/6/9 14:05
+ * @Version 1.0
+ */
+public class SocketListenerEventConstant {
+    /**
+     * 鉴权监听
+     */
+    public static final String AUTH_EVENT = "AUTH_EVENT";
+    /**
+     * 设备监听
+     */
+    public static final String DEVICE_EVENT = "DEVICE_EVENT";
+    /**
+     * IM监听
+     */
+    public static final String IM_EVENT = "IM_EVENT";
+
+    /**
+     * 通用指令监听
+     */
+    public static final String GENERAL_EVENT = "GENERAL_EVENT";
+
+
+}
